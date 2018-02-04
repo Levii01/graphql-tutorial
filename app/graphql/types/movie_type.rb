934de5973fceebe73401b9731d457cfa87f22d7e
Levii01 <m.sedziak01@gmail.com>
2018-02-04 19:35:44 +0100

    Add schemas relation for actors and movies

--- app/graphql/types/movie_type.rb
+++ app/graphql/types/movie_type.rb
@@ -5,6 +5,11 @@ Types::MovieType = GraphQL::ObjectType.define do
   field :title, types.String
   field :summary, types.String
   field :year, types.Int
-
-  # field :actors, Types::ActorType
+  field :actors do
+    type types[Types::ActorType]
+    argument :size, types.Int, default_value: 10
+    resolve -> (movie, args, ctx) {
+      movie.actors.limit(args[:size])
+    }
+  end
 end
