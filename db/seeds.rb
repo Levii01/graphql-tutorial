a78cb0a2731cc6b238ae1456d8d3f53aa092ab1a
Levii01 <m.sedziak01@gmail.com>
2018-02-04 18:46:01 +0100

    Fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 actors = []
 20.times { actors << FactoryBot.create(:actor) }
-actors.sample(rand(20)).each do |actor|
+
+10.times do
   movie = FactoryBot.create(:movie)
-  ActorsMovie.create!(movie: movie, actor: actor)
+  actors.sample(rand(20)+1).each { |actor| ActorsMovie.create!(movie: movie, actor: actor) }
 end
